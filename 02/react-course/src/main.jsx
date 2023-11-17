a1f8d73fe112278992bdb9bc4cfaf97923b9018f
Mateusz Majer <mateusz.majer996@gmail.com>
2023-11-17 01:39:52 +0100

    commit

--- 02/react-course/src/main.jsx
+++ 02/react-course/src/main.jsx
@@ -1,23 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = React.createElement("a", { href: "https://google.pl" }, "Test");
 
-console.log(<h1>Test</h1>);
-console.log(element);
-
-const currentDate = new Date();
-
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
-
-console.log(`Czas: ${hours}:${minutes}:${seconds}`);
-
-const timer = (
-  <div>
-    Czas:{hours}:{minutes}:{seconds}
-  </div>
+const element = (
+  <div></div>
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(timer);
+ReactDOM.createRoot(document.getElementById("root")).render(element);
