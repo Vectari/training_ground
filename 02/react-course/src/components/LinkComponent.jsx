ea33da7e944af6eeaedac9b845353aa836af2ff2
Mateusz Majer <mateusz.majer996@gmail.com>
2023-11-21 02:05:57 +0100

    props update

--- 02/react-course/src/components/LinkComponent.jsx
+++ 02/react-course/src/components/LinkComponent.jsx
@@ -1,11 +1,11 @@
-export function LinkComponent({ text, open }) {
-  const address = "https://google.pl";
+const address = "https://google.pl";
+
+export function LinkComponent({ text = address, open }) {
   const target = open ? "_blank" : null;
   const rel = open ? "noopener noreferrer" : null;
-  const isText = text ? text : address;
   return (
     <a href={address} target={target} rel={rel}>
-      {isText}
+      {text}
     </a>
   );
 }
