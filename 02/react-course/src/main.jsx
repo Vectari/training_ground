f1e752eb935e8472b2b2ab8b4ea81c317b1d1a20
Mateusz Majer <mateusz.majer996@gmail.com>
2023-11-23 00:28:58 +0100

    children

--- 02/react-course/src/main.jsx
+++ 02/react-course/src/main.jsx
@@ -1,20 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { LinkComponent } from "./components/LinkComponent.jsx";
+import App from "./App";
 
-const element = (
-  <>
-    <h1>Nasza strona!</h1>
-    <main>
-      <LinkComponent text="GOOOGLE" open={true} />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
-        nostrum ab ullam facilis, voluptas quaerat consequatur voluptatem
-        consequuntur eligendi ex maxime? Temporibus suscipit deserunt numquam
-        delectus nam minima laboriosam soluta.
-      </p>
-      <LinkComponent open={false} />
-    </main>
-  </>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
