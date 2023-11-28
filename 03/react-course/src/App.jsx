d4d0f67675b06130943c7af6dc67c82a8b21cbec
Mateusz Majer <mateusz.majer996@gmail.com>
2023-11-28 23:21:05 +0100

    commit

--- 03/react-course/src/App.jsx
+++ 03/react-course/src/App.jsx
@@ -1,11 +1,15 @@
 function App() {
+  function handleClick() {
+    alert("klik!")
+  }
+
   return (
     <>
       <h1>Gwiezdne Wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
       <h2>Fabuła:</h2>
       <p>Strzały i kosmos.</p>
-      <button onClick={() => alert("klik!")}>SPOILER</button>
+      <button onClick={handleClick}>SPOILER</button>
       <p>Gwiezdne Wojny są słabe.</p>
     </>
   );
