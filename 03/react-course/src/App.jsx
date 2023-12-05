e9fa1f3a0d42933be99df2cf4c9fe10b4de6bafb
Mateusz Majer <mateusz.majer996@gmail.com>
2023-12-05 21:56:54 +0100

    usestate

--- 03/react-course/src/App.jsx
+++ 03/react-course/src/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
+
 function App() {
-  function handleClick() {
-    alert("klik!");
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [closeWarningClick, setCloseWarningClick] = useState(true);
+
+  function handleShowSpoilerClick() {
+    setIsSpoilerShown(true);
+    console.log("handleShowSpoilerClick!");
+    setCloseWarningClick(false);
   }
+
+  function handleCloseWarningClick() {
+    setCloseWarningClick(false);
+  }
+
+  console.log("app");
 
   return (
     <>
       <h1>Gwiezdne Wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
       <h2>Fabuła:</h2>
+      {closeWarningClick && (
+        <p>
+          Uwaga! Opis zawiera spoiler{" "}
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
       <p>Strzały i kosmos.</p>
-      <button onClick={handleClick}>SPOILER</button>
-      <p>Gwiezdne Wojny są słabe!!!</p>
+
+      {isSpoilerShown ? (
+        <p>Gwiezdne Wojny są słabe!!!</p>
+      ) : (
+        <button onClick={handleShowSpoilerClick}>SPOILER</button>
+      )}
     </>
   );
 }
