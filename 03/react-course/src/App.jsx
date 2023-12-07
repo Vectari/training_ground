f7da252caf301f9a858f3c22f206ad5bacb31f66
Mateusz Majer <mateusz.majer996@gmail.com>
2023-12-07 22:56:43 +0100

    state update

--- 03/react-course/src/App.jsx
+++ 03/react-course/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   const [isSpoilerShown, setIsSpoilerShown] = useState(false);
   const [closeWarningClick, setCloseWarningClick] = useState(true);
+  const [numberOfLikes, setNumberOfLikes] = useState(50);
 
   function handleShowSpoilerClick() {
     setIsSpoilerShown(true);
@@ -13,10 +14,23 @@ function App() {
     setCloseWarningClick(false);
   }
 
+  function handleLikeButtonClick() {
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+  }
+
+  function handleLoveButtonClick() {
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+  }
+
   return (
     <>
       <h1>Gwiezdne Wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
+      <h2>Licznik polubień: {numberOfLikes}</h2>
+      <button onClick={handleLikeButtonClick}>Lubię to!</button>
+      <button onClick={handleLoveButtonClick}>Kocham to!</button>
       <h2>Fabuła:</h2>
       {closeWarningClick && (
         <p>
