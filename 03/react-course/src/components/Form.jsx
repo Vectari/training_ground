bbd7d09b117227df5c6c0ff722b4632246864c22
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-02 23:05:51 +0100

    add form component

---
+++ 03/react-course/src/components/Form.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export function Form() {
+  const [review, setReview] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [textAreaValue, setTextAreaValue] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const author = inputValue;
+    const text = textAreaValue;
+
+    setReview({ author, text });
+  }
+
+  return (
+    <>
+      {review && (
+        <article>
+          <strong>{inputValue}</strong>
+          <p>{textAreaValue}</p>
+        </article>
+      )}
+      <h2>Dodaj recenzję</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="author">Autor</label>
+        </div>
+        <input
+          type="text"
+          name="author"
+          id="author"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+        <div>
+          <div>
+            <label htmlFor="text">Tekst</label>
+          </div>
+          <textarea
+            name="text"
+            id="text"
+            value={textAreaValue}
+            onChange={(event) => {
+              setTextAreaValue(event.target.value);
+            }}
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          disabled={inputValue === "" || textAreaValue === ""}
+        >
+          Dodaj
+        </button>
+      </form>
+    </>
+  );
+}
