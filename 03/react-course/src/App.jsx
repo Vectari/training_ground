ef449874f6774137ca12386d58af3f4711528afe
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-04 23:36:48 +0100

    commit

--- 03/react-course/src/App.jsx
+++ 03/react-course/src/App.jsx
@@ -8,8 +8,6 @@ function App() {
       <h1>Gwiezdne Wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
       <LikesCounter number={50} />
-      <LikesCounter number={70} />
-      <LikesCounter number={100} />
       <Plot />
       <Form />
     </>
