ed64a1e5e5dbf78933a40b038071b963e76cc5b0
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-14 12:56:53 +0100

    scss

--- 04/react-course/src/main.jsx
+++ 04/react-course/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import "./styles.css";
+import "./styles.scss";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
