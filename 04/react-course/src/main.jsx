22f9e492193cd75516b6347f00db326c3a2b24a3
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-22 22:05:42 +0100

    install tailwind css

--- 04/react-course/src/main.jsx
+++ 04/react-course/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import "./styles.scss";
+import "./styles.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
