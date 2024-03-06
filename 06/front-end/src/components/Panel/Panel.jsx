f4eb81999de752ddaab7ace901417ae2ca0ee7c1
Vectari <mateusz.majer996@gmail.com>
2024-03-06 22:57:53 +0100

    update

--- 06/front-end/src/components/Panel/Panel.jsx
+++ 06/front-end/src/components/Panel/Panel.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useMemo } from "react";
 import { List } from "../List/List";
 import { Form } from "../Form/Form";
-import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 import { FilterButton } from "../FilterButton/FilterButton";
 import { Info } from "../Info/Info";
 import styles from "./Panel.module.css";
@@ -9,10 +8,9 @@ import { getCategoryInfo } from "../../utils/getCategoryInfo";
 
 const url = "http://localhost:3000/words";
 
-export function Panel() {
+export function Panel({ onError }) {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
@@ -20,20 +18,29 @@ export function Panel() {
     const params = selectedCategory ? `?category=${selectedCategory}` : "";
 
     fetch(`${url}${params}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Loading data error!");
+      })
       .then((res) => {
         if (!isCanceled) {
           setData(res);
           setIsLoading(false);
         }
-      });
+      })
+      .catch(onError);
 
     return () => {
       isCanceled = true;
     };
-  }, [selectedCategory]);
+  }, [selectedCategory, onError]);
 
-  const categoryInfo = useMemo(() => getCategoryInfo(selectedCategory), [selectedCategory]);
+  const categoryInfo = useMemo(
+    () => getCategoryInfo(selectedCategory),
+    [selectedCategory]
+  );
 
   function handleFormSubmit(formData) {
     fetch(url, {
@@ -62,12 +69,7 @@ export function Panel() {
           throw new Error("Deleting error!");
         }
       })
-      .catch((e) => {
-        setError(e.message);
-        setTimeout(() => {
-          setError(null);
-        }, 3000);
-      });
+      .catch(onError);
   }
 
   function handleFilterClick(category) {
@@ -80,7 +82,6 @@ export function Panel() {
 
   return (
     <>
-      {error && <ErrorMessage>{error}</ErrorMessage>}
       <section className={styles.section}>
         <Info>{categoryInfo}</Info>
         <Form onFormSunbmit={handleFormSubmit} />
