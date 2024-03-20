2ddfe12dab3499984b4cc1a05729b682b8603599
Vectari <mateusz.majer996@gmail.com>
2024-03-20 22:38:13 +0100

    update

--- 07/src/components/Article/Article.jsx
+++ 07/src/components/Article/Article.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
 import { Recipe } from "../Recipe/Recipe";
 import { Button } from "../Button/Button";
+import { IsLoggedInStateContext } from "../../context/IsLoggedInStateContext";
 
 export function Article() {
+  const [isLoggedIn, setIsLoggedIn] = useContext(IsLoggedInStateContext);
+
   return (
     <article>
       <Recipe />
-      <Button onClick={() => alert("Przepis polubiony!")}>Lubię to!</Button>
+      {isLoggedIn ? (
+        <Button onClick={() => alert("Przepis polubiony!")}>Lubię to!</Button>
+      ) : (
+        <Button onClick={() => setIsLoggedIn(true)}>Zaloguj</Button>
+      )}
     </article>
   );
 }
