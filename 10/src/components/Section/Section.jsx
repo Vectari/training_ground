4fc391a5727ff2988d57b1731a0ee815d709e030
Vectari <mateusz.majer996@gmail.com>
2024-04-14 22:25:44 +0200

    new lesson - portal

---
+++ 10/src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+import "./Section.css";
+
+export function Section() {
+    return (
+        <section>
+            <h2>Nie czekaj!</h2>
+            <h3>Otwórz modal!</h3>
+            <button>Otwórz</button>
+        </section>
+    );
+}
