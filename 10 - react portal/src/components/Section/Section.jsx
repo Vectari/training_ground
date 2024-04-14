150067f050787fcda68e9e9afcfe1833dbcf9aaf
Vectari <mateusz.majer996@gmail.com>
2024-04-14 22:53:48 +0200

    name change

--- 10 - react portal/src/components/Section/Section.jsx
+++ 10 - react portal/src/components/Section/Section.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import { Modal } from "../Modal/Modal";
 import "./Section.css";
 
 export function Section() {
   const [isModalShown, setIsModalShown] = useState(false);
+  const modal = createPortal(
+    <Modal onClose={() => setIsModalShown(false)} />,
+    document.body
+  );
 
   return (
     <section>
       <h2>Nie czekaj!</h2>
       <h3>Otwórz modal!</h3>
       <button onClick={() => setIsModalShown(true)}>Otwórz</button>
-      {isModalShown && <Modal onClose={() => setIsModalShown(false)} />}
+      {isModalShown && modal}
     </section>
   );
 }
