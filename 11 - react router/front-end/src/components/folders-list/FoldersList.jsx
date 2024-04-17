7cff19a1d4e0bf3facc6302c4784f6d8f669d291
Vectari <mateusz.majer996@gmail.com>
2024-04-17 23:50:31 +0200

    add new folder

--- 11 - react router/front-end/src/components/folders-list/FoldersList.jsx
+++ 11 - react router/front-end/src/components/folders-list/FoldersList.jsx
@@ -3,45 +3,60 @@ import { Folder } from "../folder/Folder";
 import { Title } from "../title/Title";
 import { TopBar } from "../top-bar/TopBar";
 import { AddNewButton } from "../add-new-button/AddNewButton";
-import { NavLink, useLoaderData } from "react-router-dom";
+import { NavLink, useLoaderData, Form } from "react-router-dom";
 
 const Folders = ({ children }) => (
-    <div className={styles["folders-column"]}>{children}</div>
+  <div className={styles["folders-column"]}>{children}</div>
 );
 const UserCreatedFolders = ({ children }) => (
-    <div role="list" className={styles["folders-list"]}>
-        {children}
-    </div>
+  <div role="list" className={styles["folders-list"]}>
+    {children}
+  </div>
 );
 
+export async function createFolder(args) {
+  const data = await args.request.formData();
+    const folderName = data.get("folder-name");
+      return fetch("http://localhost:3000/folders", {
+        method: "POST",
+        body: JSON.stringify({
+            name: folderName
+        }),
+        headers: {
+            "Content-type": "application/json",
+        }
+      });
+}
+
 const FoldersList = () => {
-    const folders = useLoaderData();
+  const folders = useLoaderData();
 
-    return (
-        <Folders>
-            <TopBar>
-                <input
-                    className={styles["new-folder-input"]}
-                    type="text"
-                    placeholder="Nazwa folderu"
-                />
-                <AddNewButton type="submit">+</AddNewButton>
-            </TopBar>
+  return (
+    <Folders>
+      <TopBar>
+        <Form method="POST" action="/">
+          <input
+            className={styles["new-folder-input"]}
+            type="text"
+            placeholder="Nazwa folderu"
+            name="folder-name"
+          />
+          <AddNewButton type="submit">+</AddNewButton>
+        </Form>
+      </TopBar>
 
-            <Title>Foldery</Title>
-            <UserCreatedFolders>
-                {folders.map((folder) => (
-                    <NavLink key={folder.id} to={`/notes/${folder.id}`}>
-                        {({ isActive }) => {
-                            return (
-                                <Folder active={isActive}>{folder.name}</Folder>
-                            );
-                        }}
-                    </NavLink>
-                ))}
-            </UserCreatedFolders>
-        </Folders>
-    );
+      <Title>Foldery</Title>
+      <UserCreatedFolders>
+        {folders.map((folder) => (
+          <NavLink key={folder.id} to={`/notes/${folder.id}`}>
+            {({ isActive }) => {
+              return <Folder active={isActive}>{folder.name}</Folder>;
+            }}
+          </NavLink>
+        ))}
+      </UserCreatedFolders>
+    </Folders>
+  );
 };
 
 export default FoldersList;
--- 11 - react router/front-end/src/main.jsx
+++ 11 - react router/front-end/src/main.jsx
@@ -4,41 +4,41 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
 import NotesList from "./components/notes-list/NotesList";
 import { Note } from "./components/note/Note";
+import { createFolder } from "./components/folders-list/FoldersList";
 
 const router = createBrowserRouter([
-    {
-        element: <App />,
-        path: "/",
-        loader: () => {
-            return fetch("http://localhost:3000/folders");
+  {
+    element: <App />,
+    path: "/",
+    action: createFolder,
+    loader: () => {
+      return fetch("http://localhost:3000/folders");
+    },
+    children: [
+      {
+        path: "notes/:folderId",
+        element: <NotesList />,
+        loader: ({ params }) => {
+          return fetch(
+            `http://localhost:3000/notes?folderId=${params.folderId}`
+          );
         },
         children: [
-            {
-                path: "notes/:folderId",
-                element: <NotesList />,
-                loader: ({ params }) => {
-                    return fetch(
-                        `http://localhost:3000/notes?folderId=${params.folderId}`
-                    );
-                },
-                children: [
-                    {
-                        path: `note/:noteId`,
-                        element: <Note />,
-                        loader: ({ params }) => {
-                            return fetch(
-                                `http://localhost:3000/notes/${params.noteId}`
-                            );
-                        },
-                    },
-                ],
+          {
+            path: `note/:noteId`,
+            element: <Note />,
+            loader: ({ params }) => {
+              return fetch(`http://localhost:3000/notes/${params.noteId}`);
             },
+          },
         ],
-    },
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
