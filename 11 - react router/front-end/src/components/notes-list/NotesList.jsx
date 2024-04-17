370cc6879188035bc6ab732038839d125c7d83e4
Vectari <mateusz.majer996@gmail.com>
2024-04-17 23:52:30 +0200

    add new note

--- 11 - react router/front-end/src/components/notes-list/NotesList.jsx
+++ 11 - react router/front-end/src/components/notes-list/NotesList.jsx
@@ -4,48 +4,60 @@ import { AddNewButton } from "../add-new-button/AddNewButton";
 import { TopBar } from "../top-bar/TopBar";
 import { ShortNote } from "../short-note/ShortNote";
 import { Note } from "../note/Note";
-import { useLoaderData, NavLink, Outlet } from "react-router-dom";
+import { useLoaderData, NavLink, Outlet, Form } from "react-router-dom";
 
 const NotesContainer = ({ children }) => (
-    <div className={styles["notes-container"]}>{children}</div>
+  <div className={styles["notes-container"]}>{children}</div>
 );
 
 const Notes = ({ children }) => (
-    <div className={styles["notes-list"]} role="list">
-        {children}
-    </div>
+  <div className={styles["notes-list"]} role="list">
+    {children}
+  </div>
 );
 
+export function createNote({ params }) {
+  return fetch("http://localhost:3000/notes ", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      title: "Nowa notatka",
+      body: "Treść notatki",
+      folderId: Number(params.folderId),
+    }),
+  });
+}
+
 const NotesList = () => {
-    const notes = useLoaderData();
+  const notes = useLoaderData();
 
-    return (
-        <NotesContainer>
-            <Notes>
-                <TopBar>
-                    <Title>Notatki</Title>
+  return (
+    <NotesContainer>
+      <Notes>
+        <TopBar>
+          <Title>Notatki</Title>
+          <Form method="POST">
+            <AddNewButton>+</AddNewButton>
+          </Form>
+        </TopBar>
 
-                    <AddNewButton>+</AddNewButton>
-                </TopBar>
-
-                {notes.map((note) => (
-                    <NavLink
-                        key={note.id}
-                        to={`/notes/${note.folderId}/note/${note.id}`}
-                    >
-                        {({ isActive }) => (
-                            <ShortNote
-                                active={isActive}
-                                role="listitem"
-                                note={note}
-                            ></ShortNote>
-                        )}
-                    </NavLink>
-                ))}
-            </Notes>
-            <Outlet />
-        </NotesContainer>
-    );
+        {notes.map((note) => (
+          <NavLink key={note.id} to={`/notes/${note.folderId}/note/${note.id}`}>
+            {({ isActive }) => (
+              <ShortNote
+                active={isActive}
+                role="listitem"
+                note={note}
+              ></ShortNote>
+            )}
+          </NavLink>
+        ))}
+      </Notes>
+      <Outlet />
+    </NotesContainer>
+  );
 };
 
 export default NotesList;
